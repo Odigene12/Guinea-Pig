1eabcf888e66e5136a2decfb8bb60aad112beb55
Odie Joseph <Odie Joseph>
2016-03-02 19:52:21 -0600

    declared variables for ID and Class

---
+++ events.js
@@ -0,0 +1,12 @@
+// Create variables for all id's 
+var output = document.getElementById("output-target");
+var keypress = document.getElementById("keypress-input");
+var page-title = document.getElementById("page-title");
+var page-header= document.getElementById("page-header");
+var guinea = document.getElementById("guinea-pig");
+var addColor = document.getElementById("add-color");
+var hulkify = document.getElementById("make-large");
+var capture = document.getElementById("add-border");
+var round = document.get getElementById("add-rounding");
+
+var article = document.getElementByclassName("article")
