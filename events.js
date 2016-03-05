05de20b70e2fa9d3616af1504b338330138ea1bf
Odie Joseph <Odie Joseph>
2016-03-05 12:09:30 -0600

    Finished exercise

--- events.js
+++ events.js
@@ -8,6 +8,7 @@ var colorBtn = document.getElementById("add-color");
 var hulkifyBtn = document.getElementById("make-large");
 var captureBtn = document.getElementById("add-border");
 var roundBtn = document.getElementById("add-rounding");
+var Btns = document.getElementsByTagName("button")
 
 // Create variable for class
 var article = document.getElementsByClassName("article-section")
@@ -28,4 +29,27 @@ pageTitle.addEventListener("mouseout", function(){
 
 colorBtn.addEventListener("click", function(){
 	guineaBtn.classList.toggle("text");
+});
+
+hulkifyBtn.addEventListener("click", function(){
+	guinea-pig.classList.toggle("Large");
+})
+
+captureBtn.addEventListener("click", function(){
+	guinea-pig.classList.toggle("add-border");
+})
+
+// Created for loop that will go into all articles and bold the first one and italics the last one in array.
+for(var i=0; i < article.length; i++){
+	article[0].classList.add("bold")
+	article[article.length -1].classList.add("italics")
+};
+
+// Created for loop that will make all buttons blocked.
+for(var i=0; i < Btns.length; i++){
+	Btns[i].classList.add("block");
+}
+
+keypress.addEventListener("keyup", function(event){
+	output.innerHTML = keypress.value
 });
